316c18a02e5fcddc0c1f401e04b3312daf6de8d7
Julius <julius@uusinarkaus.com>
2016-12-13 13:20:35 +0200

    Added front end for viewing quotes

--- server.js
+++ server.js
@@ -7,9 +7,22 @@ var cron = require('node-cron');
 var fs = require('fs');
 
 
+
 // replace the value below with the Telegram token you receive from @BotFather
 var token = process.env.API_TOKEN
 
+// var token = '276498674:AAGX4QCN9PDjGbcMx0iwLDr3fw8dRYNNGJg';
 var bot = new TelegramBot(token, {polling: true});
 
+var mongoose = require('mongoose');
+
+var url = process.env.MONGOLAB_URL || 'mongodb://localhost/quotes';
+mongoose.connect(url);
+
+// mongoose.connect('mongodb://localhost/quotes');
+
+
+
 require('./app/commands')(bot);
+// require('./bin/www');
+
