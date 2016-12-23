f104395c587de8105377c3ffd865f4b4a3ce2b3b
Julius <julius@uusinarkaus.com>
2016-12-24 00:13:48 +0200

    Fixed bug that crashes app

--- app/bot.js
+++ app/bot.js
@@ -5,7 +5,6 @@
 module.exports = function (bot) {
     var db = require('./quotes');
     var config = require('../config');
-    var klusteri = require('./klusteri');
 
 
     function addGroup(chatId) {
