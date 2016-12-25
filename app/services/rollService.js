64d25f232c902941664e696a1022e6b8b8904e07
Julius <julius@uusinarkaus.com>
2016-12-25 23:16:48 +0200

    /roll always adds trailing zeroes

--- app/services/rollService.js
+++ app/services/rollService.js
@@ -5,19 +5,32 @@ var botOutput = require('../botOutput');
 
 function roll(msg, match){
     var input = match[4];
+    var length = 8;
     if(!isNaN(match[4])){
+        var output = 0;
+        length = input.toString().length;
+        console.log(length);
         if (input > 0){
-            botOutput.sendMessage(msg, randomIntFromInterval(1, input));
+            output = randomIntFromInterval(1, input);
         }
     } else {
-        botOutput.sendMessage(msg, randomIntFromInterval(1, 10000000));
+        output = randomIntFromInterval(1, 99999999);
     }
+
+    output = pad(output, length);
+    botOutput.sendMessage(msg, output);
 
 }
 
 function randomIntFromInterval(min,max)
 {
     return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
 module.exports = {
