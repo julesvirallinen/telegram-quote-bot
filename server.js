089335a32ab0e88d982a958f080abd57078fbc64
Julius Uusinarkaus <julius@uusinarkaus.com>
2017-02-10 20:48:31 +0200

    Made processing errors a bit better
    hotfix for fixing crashes

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var mongoose = require('mongoose');
 var url = process.env.MONGOLAB_URL || 'mongodb://localhost/quotes';
 mongoose.connect(url);
 
+process.on('uncaughtException', function (err) {
+    console.error((new Date).toUTCString() + ' uncaughtException:', err.message)
+    console.error(err.stack)
+    process.exit(1)
+})
+
+
+
 
 require('./app/bot/commandController');
 // require('./app/service/timeService');
