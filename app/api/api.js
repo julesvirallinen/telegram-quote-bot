1f147530ee76d9befd7c45be0342ec5925067c3d
cxcorp <joona.o.heikkila@gmail.com>
2017-04-01 22:01:15 +0300

    Use json() instead of all() for API JSON header

--- app/api/api.js
+++ app/api/api.js
@@ -5,29 +5,21 @@ var app = express();
 var db = require('../schemas');
 var config = require('../config');
 
-app.all('/api/*', (req, res, next) => {
-    res.set('Content-Type', 'application/json');
-    next();
-});
-
 app.get("/api/:chatId/stats", (req, res) => {
     if (!req.params.chatId) {
-        res.status(400);
-        res.send({ error: 'Missing chat ID' });
+        res.status(400).json({ error: 'Missing chat ID' });
         return;
     }
 
     db.Group.findOne({ chatId: req.params.chatId }, (err, arr) => {
         if (err) {
             console.error("shit brok", err);
-            res.status(500);
-            res.send({ error: 'shit brok' });
+            res.status(500).json({ error: 'shit brok' });
             return;
         }
 
         db.Quote.count({ group: arr._id }, (err, count) => {
-            res.status(200);
-            res.send({
+            res.status(200).json({
                 data: {
                     quotes_requested: arr.counts.requests,
                     quotes_returned: arr.counts.returned,
@@ -39,8 +31,7 @@ app.get("/api/:chatId/stats", (req, res) => {
 });
 
 app.all('/api/*', (req, res, next) => {
-    res.status(404);
-    res.send({ error: 'not found' });
+    res.status(404).json({ error: 'not found' });
 });
 
 if (config.api.enabled) {
